d9a0d1ea8c0d2017c3fc5bb95255ea08ee8ae544
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-02-13 22:59:37 +0200

    Creating main class; Adding simple printline

---
+++ src/main/java/machine/CoffeeMachine.java
@@ -0,0 +1,19 @@
+package machine;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        String[] instructions = {
+                "Starting to make a coffee",
+                "Grinding coffee beans",
+                "Boiling water",
+                "Mixing boiled water with crushed coffee beans",
+                "Pouring coffee into the cup",
+                "Pouring some milk into the cup",
+                "Coffee is ready!"
+        };
+
+        for (String instruction : instructions) {
+            System.out.println(instruction);
+        }
+    }
+}
