3f9e0293c929aad510c4144db38171945e02a651
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-02-13 23:27:38 +0200

    Created cups of coffee query

--- src/main/java/machine/CoffeeMachine.java
+++ src/main/java/machine/CoffeeMachine.java
@@ -1,19 +1,24 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        AskCupsOfCoffee();
+    }
+    public static void AskCupsOfCoffee(){
+        System.out.println("Write how many cups of coffee you will need:");
+        Scanner scanner = new Scanner(System.in);
+        int cups = scanner.nextInt();
         String[] instructions = {
-                "Starting to make a coffee",
-                "Grinding coffee beans",
-                "Boiling water",
-                "Mixing boiled water with crushed coffee beans",
-                "Pouring coffee into the cup",
-                "Pouring some milk into the cup",
-                "Coffee is ready!"
+                "For 25 cups of coffee you will need:",
+                (cups * 200) + " ml of water",
+                (cups * 50) + "ml of milk",
+                (cups * 15) + "g of coffee beans",
         };
-
         for (String instruction : instructions) {
             System.out.println(instruction);
         }
+
     }
 }
