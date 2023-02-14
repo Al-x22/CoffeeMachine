e46a669d37ebbed1b8d53ee78825b40ac307f8da
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-02-14 20:49:51 +0200

    Prints available coffee after ingredient input

--- src/main/java/machine/CoffeeMachine.java
+++ src/main/java/machine/CoffeeMachine.java
@@ -4,21 +4,39 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        AskCupsOfCoffee();
+        int water = askMlOfWater();
+        int milk = askMlOfMilk();
+        int coffee = askGramsOfCoffee();
+        int wantedCups = askCupsOfCoffee();
+
+        int availableCups = Math.min(Math.min(water/200, milk/50), coffee/15);
+
+        if (wantedCups > availableCups) System.out.println("No, I can make only " + availableCups + " cup(s) of coffee");
+        else if (wantedCups < availableCups)
+            System.out.println("Yes, I can make that amount of coffee (and even " + (availableCups - wantedCups) +
+                    " more than that)");
+        else System.out.println("Yes, I can make that amount of coffee");
+
+
     }
-    public static void AskCupsOfCoffee(){
-        System.out.println("Write how many cups of coffee you will need:");
+    public static int askQuestion(String question){
+        System.out.println(question);
         Scanner scanner = new Scanner(System.in);
-        int cups = scanner.nextInt();
-        String[] instructions = {
-                "For 25 cups of coffee you will need:",
-                (cups * 200) + " ml of water",
-                (cups * 50) + "ml of milk",
-                (cups * 15) + "g of coffee beans",
-        };
-        for (String instruction : instructions) {
-            System.out.println(instruction);
-        }
+
+        return scanner.nextInt();
+    }
+    public static int askMlOfWater(){
+        return askQuestion("Write how many ml of water the coffee machine has:");
+    }
+    public static int askMlOfMilk(){
+        return askQuestion("Write how many ml of milk the coffee machine has:");
+    }
+    public static int askGramsOfCoffee(){
+        return askQuestion("Write how many grams of coffee beans the coffee machine has:");
+    }
+
+    public static int askCupsOfCoffee(){
+        return askQuestion("Write how many cups of coffee you will need:");
 
     }
 }
